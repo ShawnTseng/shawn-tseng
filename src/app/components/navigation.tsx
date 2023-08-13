5f4eba5176a861aa702c5753df7c63cc94f2d95b
ShawnTseng <shawntseng40@gmail.com>
2023-08-13 09:46:23 +0800

    chore: minimize the quantity of class

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,16 +1,13 @@
 export default function Navigation() {
     return (
         <header className="bg-white border-b border-sky-200">
-            <nav className="mx-auto p-6 lg:px-8 flex max-w-7xl items-center justify-between" aria-label="Navigation">
-                <div className="flex lg:flex-1">
-                    <span>Shawn TSENG</span>
-                </div>
-                <div className="mr-2">
-                    <button className="btn-sky mr-2">Resume</button>
-                    <button className="btn-sky mr-2">Portfolio</button>
-                    <button className="btn-sky mr-2">About me</button>
-                    <button className="btn-sky">Contact me</button>
-                </div>
+            <nav className="p-6 lg:px-8 flex max-w-7xl items-center">
+                <span>Shawn TSENG</span>
+                <div className="flex-1"></div>
+                <button className="btn-sky mr-2">Resume</button>
+                <button className="btn-sky mr-2">Portfolio</button>
+                <button className="btn-sky mr-2">About me</button>
+                <button className="btn-sky">Contact me</button>
             </nav>
         </header>
     )
