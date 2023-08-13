32606bb3993e470b3ebd8e57241fb528dcc27564
ShawnTseng <shawntseng40@gmail.com>
2023-08-13 09:41:38 +0800

    feat: add prototype of navigation and color palatte

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,25 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'sky': {
+        '50': '#f2fdff', 
+        '100': '#e6f9fc', 
+        '200': '#c0eefa', 
+        '300': '#98e1f5', 
+        '400': '#51c6f0', 
+        '500': '#0ea5e9', 
+        '600': '#0a8cd1', 
+        '700': '#076bad', 
+        '800': '#06508c', 
+        '900': '#033669', 
+        '950': '#011d42'
+    }
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
