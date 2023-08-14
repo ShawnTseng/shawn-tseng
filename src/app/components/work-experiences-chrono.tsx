e9c7bedfc6b117e10e96289260273f268227b089
ShawnTseng <shawntseng40@gmail.com>
2023-08-14 21:09:53 +0800

    add second work exp

--- src/app/components/work-experiences-chrono.tsx
+++ src/app/components/work-experiences-chrono.tsx
@@ -12,24 +12,52 @@ type WorkExperience = {
 }
 
 export default function WorkExperiencesChrono() {
+    const setWorkExperienceDurationInfo = (workExp: WorkExperience) => {
+        const startDate = workExp.startDate.format('yyyy/MM');
+        const endDate = workExp.endDate.format('yyyy-MM') === moment().format('yyyy-MM') ? 'Current' : workExp.endDate.format('yyyy/MM');
+        const years = workExp.endDate.diff(workExp.startDate, 'years');
+        const months = workExp.endDate.diff(workExp.startDate, 'months') - years * 12;
+        const yearString = years > 0 ? `${years} year${years > 1 ? 's' : ''}` : '';
+        const monthString = months > 0 ? `${months} month${months > 1 ? 's' : ''}` : '';
+        const conjunction = yearString && monthString ? ' and ' : '';
+        const duration = `${startDate} - ${endDate}(${yearString}${conjunction}${monthString})`;
+        workExp.years = years;
+        workExp.months = months;
+        workExp.duration = duration;
+    }
+
     const vertiv: WorkExperience = {
         startDate: moment('2021-03'),
-        endDate: moment(Date.now()),
+        endDate: moment(),
         years: 0,
         months: 0,
         duration: '',
         companyName: 'VERTIV',
         jobTitle: 'Senior Software Engineer'
     }
-    vertiv.years = vertiv.endDate.diff(vertiv.startDate, 'years');
-    vertiv.months = vertiv.endDate.diff(vertiv.startDate, 'months') - vertiv.years * 12;
-    vertiv.duration = `${vertiv.startDate.format('yyyy/MM')} - Current(${vertiv.years} year${vertiv.years > 1 ? 's' : ''} and ${vertiv.months} month${vertiv.months > 1 ? 's' : ''})`;
+    setWorkExperienceDurationInfo(vertiv);
+
+    const atGames: WorkExperience = {
+        startDate: moment('2020-08'),
+        endDate: moment('2021-03'),
+        years: 0,
+        months: 0,
+        duration: '',
+        companyName: 'ATGames',
+        jobTitle: 'Senior Front End Developer'
+    }
+    setWorkExperienceDurationInfo(atGames);
 
     const items = [
         {
             title: vertiv.duration,
             cardTitle: vertiv.companyName,
             cardSubtitle: vertiv.jobTitle,
+        },
+        {
+            title: atGames.duration,
+            cardTitle: atGames.companyName,
+            cardSubtitle: atGames.jobTitle,
         }
     ];
 
