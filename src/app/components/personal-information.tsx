57cf4b123617e92c7719abf1a5337c8787ed1d92
ShawnTseng <shawntseng40@gmail.com>
2023-08-15 14:29:35 +0800

    update personal info

--- src/app/components/personal-information.tsx
+++ src/app/components/personal-information.tsx
@@ -1,5 +1,15 @@
 export default function PersonalInformation() {
     return (
-        <div>PersonalInformation</div>
+        <div>
+            <div>
+                Name: Shawn TSENG
+            </div>
+            <div>
+                Contact Number: +886 910-308-147
+            </div>
+            <div>
+                E-mail: shawntseng40@gmail.com
+            </div>
+        </div>
     )
 }
