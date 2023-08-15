2e942c95879b94a9a3fd13b0a06405dfc088a3b7
ShawnTseng <shawntseng40@gmail.com>
2023-08-15 23:11:23 +0800

    chore

--- src/app/components/skills.tsx
+++ src/app/components/skills.tsx
@@ -24,10 +24,11 @@ export default function Skills() {
     <>
       <div className="relative max-w-lg w-full bg-sky-50 mx-4 px-5 py-3 rounded-lg">
         <div className="w-full my-6">
+          {/* test */}
           <span className="block text-sm font-semibold text-sky-950">HTML</span>
           <div className="h-2 w-full rounded-md mt-2 bg-sky-950">
-            {/* TODO:remove skill-per, html */}
-            <span className={`skill-per ${htmlWidth[0]} relative block h-full rounded-md opacity-0 bg-sky-500 bar-percent delay-100`}>
+            {/* TODO:remove skill-per */}            
+            <span className={`skill-per ${htmlWidth[0]} relative block h-full rounded-md opacity-0 bg-sky-500`}>
               <span className="absolute right-[-14px] top-[-35px] text-sm font-medium text-sky-50 rounded bg-sky-500 px-[6px] py-[3px]
                                before:content-[''] before:absolute before:left-1/2 before:bottom-[-5px] before:h-[10px] before:w-[10px] z-10 before:translate-x-[-50%] before:rotate-45 before:bg-sky-500">
                 {htmlWidth[1]}
