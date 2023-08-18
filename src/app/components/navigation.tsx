7ea89c8c878f274ca2df3ec2d15163db06d7e7c2
ShawnTseng <shawntseng40@gmail.com>
2023-08-18 15:08:21 +0800

    add logo block for entire website

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,14 +1,38 @@
+import Image from 'next/image';
+import { Poppins } from 'next/font/google';
+
+const poppinsMedium = Poppins({
+    weight: '500',
+    subsets: ['latin']
+});
+
+const poppinsBold = Poppins({
+    weight: '700',
+    subsets: ['latin']
+});
+
 export default function Navigation() {
     return (
-        <header className="bg-white border-b border-sky-200">
+        <header className="w-full h-20 px-40 py-4 shadow-md gap-[400px] flex items-center">
+            <span className='inline-flex items-center gap-4'>
+                <Image src="/favicon.ico" className='pb-1' width={33} height={36.59} alt="logo" />
+                <span className='flex items-center text-[26px] leading-[39px] gap-[1px] text-[#000000B2]'>
+                    <span className={`${poppinsMedium.className} tracking-[-0.015em]`}>
+                        SHAWN
+                    </span>
+                    <span className={`${poppinsBold.className} tracking-[0.02em]`}>
+                        TSENG
+                    </span>
+                </span>
+            </span>
+
             <nav className="p-6 lg:px-8 flex max-w-7xl items-center">
-                <span>Shawn TSENG</span>
                 <div className="flex-1"></div>
                 <button className="btn-sky mr-2">Resume</button>
                 <button className="btn-sky mr-2">Portfolio</button>
                 <button className="btn-sky mr-2">About me</button>
                 <button className="btn-sky">Contact me</button>
             </nav>
-        </header>
+        </header >
     )
 }
