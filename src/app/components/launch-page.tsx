0655390abad07396d07b2544b20abc02ed9a65d6
ShawnTseng <shawntseng40@gmail.com>
2023-08-18 16:31:29 +0800

    remain landing text block

--- src/app/components/launch-page.tsx
+++ src/app/components/launch-page.tsx
@@ -2,13 +2,12 @@ export default function LaunchPage() {
     return (
         <>
             <div className="relative h-[720px]">
-                <div className="flex-[50%]">
-                    A
+                <div className="w-1/2 h-full flex justify-center items-center">
+                    {/* TODO: add text block */}
+                    TODO
                 </div>
-                <div className="flex-[50%]">
-                    <div className="absolute left-[693px] top-0 w-[440px] h-[784px] bg-[#D9D9D966]" />
-                    <div className="absolute left-[811px] top-[176px] w-[442px] h-[522px] bg-[#D9D9D9]" />
-                </div>
+                <div className="absolute left-[693px] top-0 w-[440px] h-[784px] bg-[#D9D9D966]" />
+                <div className="absolute left-[811px] top-[176px] w-[442px] h-[522px] bg-[#D9D9D9]" />
             </div>
         </>
     )
