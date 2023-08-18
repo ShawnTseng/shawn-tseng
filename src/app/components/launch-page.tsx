9646649541f245e05522eac1647b320ceeaa9983
ShawnTseng <shawntseng40@gmail.com>
2023-08-18 16:27:50 +0800

    add landing page block

--- src/app/components/launch-page.tsx
+++ src/app/components/launch-page.tsx
@@ -1,5 +1,15 @@
 export default function LaunchPage() {
     return (
-        <div>Launch Page</div>
+        <>
+            <div className="relative h-[720px]">
+                <div className="flex-[50%]">
+                    A
+                </div>
+                <div className="flex-[50%]">
+                    <div className="absolute left-[693px] top-0 w-[440px] h-[784px] bg-[#D9D9D966]" />
+                    <div className="absolute left-[811px] top-[176px] w-[442px] h-[522px] bg-[#D9D9D9]" />
+                </div>
+            </div>
+        </>
     )
 }
