97c474c029a9ec3990c1bd6f1fad919a37262a55
ShawnTseng <shawntseng40@gmail.com>
2023-08-25 15:52:24 +0800

    chore

--- src/app/components/launch-page.tsx
+++ src/app/components/launch-page.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function LaunchPage() {
     return (
         <>
-            <div className="w-full h-[800px] flex justify-center items-center bg-gradient-to-r from-[#B9E1FF] from-20% via-[#7BC6FF] via-[90.67%] to-[#49B1FF]">
+            {/* TODO:fix gradient problem
+                background: radial-gradient(70% 79.55% at 17.54% 55.87%, rgba(185, 225, 255, 0.2) 0%, rgba(185, 225, 255, 0.8) 16.84%, rgba(123, 198, 255, 0.906667) 54.41%, #49B1FF 100%)
+            */}
+            <div className="w-full h-[calc(100vh-80px)] flex justify-center items-center 
+            bg-gradient-to-r from-[#B9E1FF] from-20% via-[#7BC6FF] via-[90.67%] to-[#49B1FF]">
                 <div className="max-w-7xl grid grid-cols-2">
                     {/* Brief Intro */}
                     <div className="flex flex-col justify-center items-start ml-14">
