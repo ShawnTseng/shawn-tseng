12930485207324ab9bd2fd909263bbac5bfcf3af
Shawn Tseng <shawntseng40@gmail.com>
2023-10-20 15:02:16 +0800

    refactor: unify poker card css

--- src/app/poker/page.tsx
+++ src/app/poker/page.tsx
@@ -141,15 +141,12 @@ export default function Poker() {
 
     return <div className="w-full flex flex-col justify-center items-center p-8">
         <div className="max-w-7xl w-full text-right">
-            <button className=" p-4 rounded-md bg-sky-500 shadow-md" onClick={dealHandToMe}>Deal Hand</button>
+            <button className="btn-sky" onClick={dealHandToMe}>Deal Hand</button>
         </div>
         <h1 className="m-8">Hand</h1>
-        <div className="max-w-7xl w-full flex justify-center gap-2">
+        <div className="max-w-7xl w-full flex justify-center poker-card-hand">
             {hand.map((card, index) => {
-                return <div key={index}
-                    className={`border-2 w-10 min-w-[40px] h-16 gap-1 
-            flex justify-center items-center 
-            rounded-md ${card.color === Colors.Red ? 'text-[#f00]' : 'text-[#000]'}`}>
+                return <div key={index} className={`poker-card ${card.color === Colors.Red ? 'poker-card-red' : 'poker-card-black'}`}>
                     <span>{card.suit}</span>
                     <span>{card.rank}</span>
                 </div>
@@ -158,12 +155,9 @@ export default function Poker() {
         <h1 className="m-8">Possible Pre-Flop Hand Combinations({handCombinations.length})</h1>
         <div className="max-w-7xl w-full grid grid-cols-12 gap-2">
             {handCombinations.map((handCombination) => {
-                return <div className="flex"> {
+                return <div className="flex poker-card-hand"> {
                     handCombination.map((card, index) => {
-                        return <div key={index}
-                            className={`border-2 w-10 min-w-[40px] h-16 gap-1 
-                                        flex justify-center items-center 
-                                        rounded-md ${card.color === Colors.Red ? 'text-[#f00]' : 'text-[#000]'}`}>
+                        return <div key={index} className={`poker-card ${card.color === Colors.Red ? 'poker-card-red' : 'poker-card-black'}`}>
                             <span>{card.suit}</span>
                             <span>{card.rank}</span>
                         </div>
@@ -173,12 +167,9 @@ export default function Poker() {
         </div>
 
         <h1 className="m-8">All cards</h1>
-        <div className="max-w-7xl w-full grid grid-cols-13 gap-1">
+        <div className="max-w-7xl w-full grid grid-cols-13 poker-card-hand">
             {deckOfCards.map((card, index) => {
-                return <div key={index}
-                    className={`border-2 w-10 min-w-[40px] h-16 gap-1 
-                    flex justify-center items-center 
-                    rounded-md ${card.color === Colors.Red ? 'text-[#f00]' : 'text-[#000]'}`}>
+                return <div key={index} className={`poker-card ${card.color === Colors.Red ? 'poker-card-red' : 'poker-card-black'}`}>
                     <span>{card.suit}</span>
                     <span>{card.rank}</span>
                 </div>
